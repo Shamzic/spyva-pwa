933652d2aed46a2be5c6c5485cce000bcb81969e
Shamzic <shamery@free.fr>
2019-12-29 17:34:49 +0100

    user interaction on list (style, delete element)

--- src/components/Element.js
+++ src/components/Element.js
@@ -5,10 +5,41 @@ class Element extends React.Component {
   constructor(props) {
     super(props);
     this.ukey = props.ukey;
+    this.state = {
+      style: {},
+      removed: false
+    };
+  }
+
+  mouseOver() {
+    var style = { color : "red", fontStyle: "italic"};
+    this.setState({style: style});
+  }
+
+  mouseOut() {
+    var style = { color : "", fontStyle: ""};
+    this.setState({style: style});
+  }
+
+  removeElem() {
+    this.setState({removed: true})
   }
 
   render() {
-    return <li>{this.props.txt}</li>
+    return (
+      this.state.removed ? null : 
+      <li style={this.state.style}
+          onMouseOver={this.mouseOver.bind(this)}
+          onMouseOut={this.mouseOut.bind(this)}>
+        <span>
+          {this.props.txt}
+        </span>
+        <button onClick={this.removeElem.bind(this)}
+                style={{margin: '10px', fontSize: '10px'}}>
+          Supprimer
+        </button>
+      </li>
+    )
   }
 }
 
--- src/components/Test.js
+++ src/components/Test.js
@@ -6,12 +6,6 @@ class Test extends React.Component {
 
   constructor(props) {
     super(props);
-    {/* this.state = { min:props.min, sec: props.sec };
-    this.timer = setInterval(() => {
-      var newState = this.decrTime(this.state);
-      console.log(this.state);
-      this.setState({min : newState.min, sec: newState.sec});
-    }, 1000); */}
     this.state = {
       elems : [],
     };
@@ -32,36 +26,13 @@ class Test extends React.Component {
   }
 
 
-  decrTime({ min, sec }) {
-    sec = sec - 1;
-    if(sec < 0) {
-      min = min - 1;
-      if(min < 0) {
-        min = 0;
-        sec = 0;
-      } else {
-        sec = 59;
-      }
-    }
-    return { min, sec };
-  }
-  formatTime({ min, sec }) {
-    if(min < 10) min = "0" + min;
-    if(sec < 10) sec = "0" + sec;
-    return `${min}:${sec}`;
-  }
-
   render() {
-  {/*  if(this.state.min === 0 && this.state.sec === 0) {
-      clearInterval(this.timer);
-      return (<div>Chrono écoulé !</div>)
-    }
-    return (<div>{this.formatTime(this.state)}</div>) */}
     return (
-    <div>
-      <button onClick={this.insertElem.bind(this)}>Insérer</button>
-      <ListeElement elems={this.state.elems}></ListeElement>
-    </div>)
+      <div>
+        <button onClick={this.insertElem.bind(this)}>Insérer</button>
+        <ListeElement elems={this.state.elems}></ListeElement>
+      </div>
+    )
   }
 }
 
